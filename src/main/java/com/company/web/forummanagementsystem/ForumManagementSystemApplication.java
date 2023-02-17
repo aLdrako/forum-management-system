e88f16eacddb34da7f1133c140a14c70ade71463
LaptopARO <petrov.argir225@gmail.com>
2023-02-17 16:02:11 +0200

    Adding Spring Initializr

---
+++ src/main/java/com/company/web/forummanagementsystem/ForumManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.company.web.forummanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/web/forummanagementsystem/ForumManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.web.forummanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
