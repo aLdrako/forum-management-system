c0f95c48aa72f1b6e03e7465e86eb8e029614388
LaptopARO <petrov.argir225@gmail.com>
2023-03-03 17:06:36 +0200

    Add Like and PostRepositoryImpl

---
+++ src/main/java/com/company/web/forummanagementsystem/models/Like.java
@@ -0,0 +1,112 @@
+package com.company.web.forummanagementsystem.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "likes")
+public class Like implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_post_id")
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", updatable = false)
+    @Column(updatable = false)
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "post_id", updatable = false)
+    @Column(updatable = false)
+    private Post post;
+
+    public Like() {
+    }
+
+    public Like(int id, User user, Post post) {
+        this.id = id;
+        this.user = user;
+        this.post = post;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Like like = (Like) o;
+        return Objects.equals(user, like.user) && Objects.equals(post, like.post);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, post);
+    }
+    //@Id
+    //@OneToOne()
+    //@JoinColumn(name = "user_id")
+    //private Long userId;
+    //@Id
+    //@OneToOne
+    //@JoinColumn(name = "post_id")
+    //private Long postId;
+//
+    //public Like() {}
+//
+    //public Long getUser() {
+    //    return userId;
+    //}
+//
+    //public void setUser(Long user) {
+    //    this.userId = user;
+    //}
+//
+    //public Long getPost() {
+    //    return postId;
+    //}
+//
+    //public void setPost(Long post) {
+    //    this.postId = post;
+    //}
+//
+    //@Override
+    //public boolean equals(Object o) {
+    //    if (this == o) return true;
+    //    if (o == null || getClass() != o.getClass()) return false;
+    //    Like like = (Like) o;
+    //    return Objects.equals(userId, like.userId) && Objects.equals(postId, like.postId);
+    //}
+//
+    //@Override
+    //public int hashCode() {
+    //    return Objects.hash(userId, postId);
+    //}
+}
---
+++ src/main/java/com/company/web/forummanagementsystem/repositories/PostRepositoryImpl.java
@@ -0,0 +1,135 @@
+package com.company.web.forummanagementsystem.repositories;
+
+import com.company.web.forummanagementsystem.exceptions.EntityNotFoundException;
+import com.company.web.forummanagementsystem.models.Like;
+import com.company.web.forummanagementsystem.models.Post;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.lang.management.ManagementPermission;
+import java.util.*;
+
+@Repository
+public class PostRepositoryImpl implements PostRepository {
+
+    private final SessionFactory sessionFactory;
+
+    public PostRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Post getById(Long id) {
+        try (Session session = sessionFactory.openSession()){
+            Post post = session.get(Post.class, id);
+            if (post == null) {
+                throw new EntityNotFoundException("Post", id);
+            }
+            return post;
+        }
+    }
+
+    @Override
+    public List<Post> getAll(Optional<Long> userId, Optional<String> title, Optional<String> sortBy, Optional<String> orderBy) {
+        try (Session session = sessionFactory.openSession()){
+            StringBuilder stringQuery = new StringBuilder("from Post");
+            Map<String, Object> queryParams = new HashMap<>();
+            List<String> filter = new ArrayList<>();
+
+            if (userId.isPresent()) {
+                filter.add(" userCreated.id = :userId ");
+                queryParams.put("userId", userId.get());
+            }
+            if (title.isPresent()) {
+                filter.add(" title like :title ");
+                queryParams.put("title", "%" + title.get() + "%");
+            }
+            if (!filter.isEmpty()) {
+                stringQuery.append(" where ").append(String.join(" and ", filter));
+            }
+            stringQuery.append(sortBy.map(this::generateSort).orElse(" order by id "));
+            stringQuery.append(orderBy.map(this::generateOrder).orElse(""));
+
+            Query<Post> query = session.createQuery(stringQuery.toString(), Post.class);
+            query.setProperties(queryParams);
+            return query.list();
+        }
+    }
+
+    private String generateOrder(String orderBy) {
+        if (orderBy.equalsIgnoreCase("desc")) {
+            return " desc ";
+        }
+        return "";
+    }
+
+    private String generateSort(String sortBy) {
+        switch (sortBy.toLowerCase()) {
+            case "title":
+                return " order by title ";
+            case "userid":
+                return " order by userCreated.id ";
+            case "datecreated":
+                return " order by dateCreated ";
+            default:
+                return "";
+        }
+    }
+
+    @Override
+    public Post create(Post post) {
+        try (Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            session.persist(post);
+            session.getTransaction().commit();
+            return post;
+        }
+    }
+
+    @Override
+    public Post update(Post post) {
+        try (Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            session.update(post);
+            session.getTransaction().commit();
+        }
+        return post;
+    }
+
+    @Override
+    public void delete(Long id) {
+        Post postToRemove = getById(id);
+        try (Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            session.remove(postToRemove);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public List<Post> getTopTenMostCommented() {
+        return null;
+    }
+
+    @Override
+    public List<Post> getTopTenMostRecent() {
+        return null;
+    }
+
+    @Override
+    public Post getPostByUserId(Long userId, Long postId) {
+        try (Session session = sessionFactory.openSession()){
+            Query<Post> query = session.createQuery("from Post where id = :postId and userCreated.id = :userId",
+                    Post.class);
+            query.setParameter("postId", postId);
+            query.setParameter("userId", userId);
+            List<Post> result = query.list();
+            if (result.size() == 0) {
+                throw new EntityNotFoundException("Post", postId, "user id", userId);
+            }
+            return result.get(0);
+        }
+    }
+}
