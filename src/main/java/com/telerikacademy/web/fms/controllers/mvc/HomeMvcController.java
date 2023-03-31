7e78b13b6cdc71bb4faba1543fc7c382a65e8c41
LaptopARO <petrov.argir225@gmail.com>
2023-03-31 12:16:02 +0300

    added: a simple count for posts and users.

--- src/main/java/com/telerikacademy/web/fms/controllers/mvc/HomeMvcController.java
+++ src/main/java/com/telerikacademy/web/fms/controllers/mvc/HomeMvcController.java
@@ -2,26 +2,38 @@ package com.telerikacademy.web.fms.controllers.mvc;
 
 import com.telerikacademy.web.fms.models.Post;
 import com.telerikacademy.web.fms.services.contracts.PostServices;
+import com.telerikacademy.web.fms.services.contracts.UserServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
 public class HomeMvcController extends BaseMvcController {
 
     private final PostServices postServices;
+    private final UserServices userServices;
 
-    public HomeMvcController(PostServices postServices) {
+    public HomeMvcController(PostServices postServices, UserServices userServices) {
         this.postServices = postServices;
+        this.userServices = userServices;
     }
 
     @ModelAttribute("mostCommentedPosts")
     public List<Post> getMostCommentedPosts() {
         return postServices.getMostCommented();
+    }
+    @ModelAttribute("usersCount")
+    public int getUsersCount() {
+        return userServices.getAll().size();
+    }
+    @ModelAttribute("postsCount")
+    public int getPostsCount() {
+        return postServices.getAll(Map.of()).size();
     }
 
     @ModelAttribute("mostRecentPosts")
